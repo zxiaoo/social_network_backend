92543cf0366a6212b1440a101e57d4f1196b0c41
zxiaoo <xiaoz@terpmail.umd.edu>
2021-09-10 23:57:16 -0700

    Added mux library for HTTP routers, and created indexes on Elasticsearch.

---
+++ elasticsearch.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"context"
+
+	"github.com/olivere/elastic/v7"
+)
+
+const (
+	ES_URL = "http://localhost:9200"
+)
+
+func readFromES(query elastic.Query, index String) (*elastic.SearchResult, error) {
+	client, err := elastic.NewClient(
+		elastic.SetURL(ES_URL)
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	searchResult, err := client.Search().
+		Index(index).
+		Query(query).
+		Pretty(true).
+		Do(contest.Background())
+	
+	if err != nil {
+		return nil, err
+	}
+
+	return searchResult, nil
+}
