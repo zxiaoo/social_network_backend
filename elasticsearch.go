79ad7e698a6aded5fe4cae30afc067f6f10efe72
zxiaoo <xiaoz@terpmail.umd.edu>
2021-09-11 00:05:41 -0700

    added function to read data from Elasticsearch.

--- elasticsearch.go
+++ elasticsearch.go
@@ -10,10 +10,10 @@ const (
 	ES_URL = "http://localhost:9200"
 )
 
-func readFromES(query elastic.Query, index String) (*elastic.SearchResult, error) {
-	client, err := elastic.NewClient(
-		elastic.SetURL(ES_URL)
-	)
+func readFromES(query elastic.Query, index string) (*elastic.SearchResult, error) {
+
+	client, err := elastic.NewClient(elastic.SetURL(ES_URL))
+
 	if err != nil {
 		return nil, err
 	}
@@ -22,7 +22,7 @@ func readFromES(query elastic.Query, index String) (*elastic.SearchResult, error
 		Index(index).
 		Query(query).
 		Pretty(true).
-		Do(contest.Background())
+		Do(context.Background())
 	
 	if err != nil {
 		return nil, err
